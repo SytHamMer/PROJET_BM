1d1baefba923f5a80620d42cedc7293d4fba9ea8
Carlyne Barrachin <carlynebarrachin@eduroam-n2ng-176159.local.univ-savoie.fr>
2023-12-22 14:36:57 +0100

    login and sign in pages

--- frontend/budget_management_application/src/app/app.component.ts
+++ frontend/budget_management_application/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, InscriptionComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'budget_management_application';
 }
---
+++ frontend/budget_management_application/src/app/inscription/inscription.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inscription',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './inscription.component.html',
+  styleUrl: './inscription.component.scss'
+})
+export class InscriptionComponent{
+
+}
