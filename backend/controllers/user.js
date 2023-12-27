4ff709293b5e9cfd5eed1c2db0946358d5b9f52d
SytHamMer <mathys.lebon1@gmail.com>
2023-12-27 16:01:07 +0100

    [UPDATE]: getByIdUser work for category_incomes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -10,12 +10,9 @@ exports.createUser = (req,res,next) => {
         email: req.body.email,
         password: req.body.password
     });
-
     user.save()
         .then(() => res.status(201).json({ message: 'User created!'}))
         .catch(error => res.status(400).json({ error }));
-
-
 
 };
 
--- backend/models/category_incomes.js
+++ backend/models/category_incomes.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const categoryIncomesSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  idUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Référence l'ID de l'utilisateur
   incomes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Income' }] // Référence les dépenses liées à cette catégorie
 });
 
