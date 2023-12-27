c66fb5db24dad116e950f76dd0f287ce2a9e0696
SytHamMer <mathys.lebon1@gmail.com>
2023-12-27 16:11:13 +0100

    [UPDATE]: getByIdUser done in both categories (need to do spending and income

--- backend/models/income.js
+++ backend/models/income.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const incomeSchema = new mongoose.Schema({
   value: { type: Number, required: true },
   date: { type: Date, default: Date.now },
-  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category_Incomes', required: true } 
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category_Incomes', required: true }
 });
 
 module.exports = mongoose.model('Income', incomeSchema);
