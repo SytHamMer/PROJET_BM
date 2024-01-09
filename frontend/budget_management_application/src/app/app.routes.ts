176c6cdf325399607e80ed6b4b966125cee0a204
SytHamMer <mathys.lebon1@gmail.com>
2024-01-09 16:58:16 +0100

    Start of user page + readme

--- frontend/budget_management_application/src/app/app.routes.ts
+++ frontend/budget_management_application/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { UserComponent } from './user/user.component';
 import { AjoutDepenseComponent } from './ajout-depense/ajout-depense.component';
 import { AjoutRevenuComponent } from './ajout-revenu/ajout-revenu.component';
 import { AjoutCategorieComponent } from './ajout-categorie/ajout-categorie.component';
@@ -10,6 +11,7 @@ export const routes: Routes = [
     { path: 'signup', component: InscriptionComponent },
     { path: '', component: LoginComponent },
     { path: 'home', component: HomepageComponent},
+    {path:'user',component: UserComponent},,
     { path: 'depense', component: AjoutDepenseComponent},
     { path: 'revenu', component: AjoutRevenuComponent},
     { path: 'categorie', component: AjoutCategorieComponent}
