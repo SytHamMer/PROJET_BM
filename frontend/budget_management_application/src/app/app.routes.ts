36e5b574263f7e797e31a6f20cb2894f7b3cd0af
lilcdx <113095135+lilcdx@users.noreply.github.com>
2024-01-09 17:04:32 +0100

    [ADD] ajout-categorie, ajout-depense, ajout-revenu : only html

--- frontend/budget_management_application/src/app/app.routes.ts
+++ frontend/budget_management_application/src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AjoutDepenseComponent } from './ajout-depense/ajout-depense.component';
+import { AjoutRevenuComponent } from './ajout-revenu/ajout-revenu.component';
+import { AjoutCategorieComponent } from './ajout-categorie/ajout-categorie.component';
 
 export const routes: Routes = [
     { path: 'signup', component: InscriptionComponent },
     { path: '', component: LoginComponent },
-    { path: 'home', component: HomepageComponent}
+    { path: 'home', component: HomepageComponent},
+    { path: 'depense', component: AjoutDepenseComponent},
+    { path: 'revenu', component: AjoutRevenuComponent},
+    { path: 'categorie', component: AjoutCategorieComponent}
 ];
