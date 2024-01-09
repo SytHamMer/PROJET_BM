f916f4e690cb68124a632b7615d3818433a64b39
SytHamMer <mathys.lebon1@gmail.com>
2024-01-09 16:58:16 +0100

    Start of user page + readme

--- frontend/budget_management_application/src/app/services/connexion.service.ts
+++ frontend/budget_management_application/src/app/services/connexion.service.ts
@@ -32,7 +32,7 @@ export class ConnexionService {
       );
   }
 
-  getUserLoggedIn$(): Observable<User | undefined> {
+  getUserLoggedIn(): Observable<User | undefined> {
     let user_id = localStorage.getItem("user_id");
     console.log(user_id);
 
