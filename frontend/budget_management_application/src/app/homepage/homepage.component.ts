01f13682fbc475a645a1e366b4ffab849caa1179
lilcdx <113095135+lilcdx@users.noreply.github.com>
2024-01-10 12:58:07 +0100

    [FIX]Affichage username homepage [ADD]SpendingPageComponent

--- frontend/budget_management_application/src/app/homepage/homepage.component.ts
+++ frontend/budget_management_application/src/app/homepage/homepage.component.ts
@@ -4,6 +4,10 @@ import { PieChartComponent } from '../piechart-budget/piechart-budget.component'
 import { AjoutDepenseComponent } from '../ajout-depense/ajout-depense.component';
 import { MatDialog } from '@angular/material/dialog';
 import { AjoutRevenuComponent } from '../ajout-revenu/ajout-revenu.component';
+import { Router, RouterLink} from "@angular/router";
+import { ConnexionService } from '../services/connexion.service';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-homepage',
@@ -14,8 +18,23 @@ import { AjoutRevenuComponent } from '../ajout-revenu/ajout-revenu.component';
 })
 export class HomepageComponent {
   isMenuPhoneHidden: boolean = true;
+  userConnected!: User;
+  errorConnexion : any | undefined;
+  username!:String;
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog,
+    private router: Router,
+    protected connexionService: ConnexionService,
+    protected userService: UserService) {}
+
+  ngOnInit(): void {
+    this.connexionService.getUserLoggedIn()
+    .subscribe(user => {
+      this.userConnected = user as User;
+      console.log(this.userConnected);
+      this.username=this.userConnected.username
+    })
+  }
 
   toggleMobileMenu() {
     this.isMenuPhoneHidden = !this.isMenuPhoneHidden;
